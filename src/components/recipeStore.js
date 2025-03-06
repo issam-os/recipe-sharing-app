66ff3669308e75ff757bc8e4dbfd67cfd9f2f1b6
issam-os <issam.bouyoussef94@gmail.com>
2025-03-06 00:51:21 +0000

    Create recipeStore.js

---
+++ src/components/recipeStore.js
@@ -0,0 +1,9 @@
+import { create } from "zustand";
+
+const useRecipeStore = create((set) => ({
+  recipes: [],
+  addRecipe: (recipe) => set((state) => ({ recipes: [...state.recipes, recipe] })),
+  setRecipes: (recipes) => set(() => ({ recipes })),
+}));
+
+export default useRecipeStore;
